ca6e8fec57a2d09b6a361a95bc09b3edd6c27783
karleandsonj <karleandsonj@gmail.com>
2023-06-23 21:06:00 -0300

    2

---
+++ menu.js
@@ -0,0 +1,6 @@
+const navicon = document.querySelector('.navicon');
+
+navicon.addEventListener('click', function() {
+  // Adicione o código que deseja executar quando o elemento for clicado
+  console.log('Elemento clicado!');
+});
