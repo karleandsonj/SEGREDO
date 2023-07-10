0882795b6b85b0019560972f44a7c6c888257f84
karleandsonj <126351266+karleandsonj@users.noreply.github.com>
2023-07-10 10:24:30 -0300

    Delete lista.js

--- lista.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $("#removerPresente").click(function() {
-    var selectedValue = $("#listaPresentes").val();
-    $("#listaPresentes option[value='" + selectedValue + "']").remove();
-  });
-});
