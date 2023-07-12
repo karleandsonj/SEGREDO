a9c2bf85a8807a730764ec32f3a43f4dbad08d8e
karleandsonj <karleandsonj@gmail.com>
2023-07-12 17:00:26 -0300

    menu 1700

--- menu.js
+++ menu.js
@@ -2,12 +2,15 @@ var navicon = document.querySelector('.navicon');
 
 // Adiciona um ouvinte de evento de clique ao elemento
 navicon.addEventListener('click', function() {
-  navicon.classList.add('active');
+  if (navicon.classList.contains('active')) {
+    navicon.classList.remove('active');
+  } else {
+    navicon.classList.add('active');
+  }
 });
 
 // Adiciona um ouvinte de evento de clique ao documento
 document.addEventListener('click', function(event) {
-  // Verifica se o elemento clicado é o .navicon ou um filho dele
   var isClickInsideNavicon = navicon.contains(event.target);
   
   // Se o clique não estiver dentro do .navicon, remove a classe "active"
